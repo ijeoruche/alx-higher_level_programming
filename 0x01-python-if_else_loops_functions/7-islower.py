064b799cfbc77f583ae339abe78313554febd37e
ijeoruche <nwanzeij@gmail.com>
2023-03-07 12:54:50 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,9 @@
+#!/usrf ord(c) > 96:
+        return True
+	    else:
+	    /bin/python3
+def islower(c):
+	    if ord(c) > 96:
+	            return True
+	        else:
+	    	    return False
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+0x01 Python If/else,Loops Functions
